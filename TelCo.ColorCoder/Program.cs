1ea3eedef9e335c620c31f42e380d33380bb0772
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-02-28 14:40:10 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -13,56 +13,52 @@ namespace TelCo.ColorCoder
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
-
-            ColorMap map = new ColorMap();
-
             int pairNumber = 4;
-            ColorPair testPair1 = map.GetColorFromPairNumber(pairNumber);
-            DisplayResultOfGetColorFromPairNumber(pairNumber, testPair1);
+            ColorPair testPair1 = CallAndPrintResultOfGetColorFromPairNumber(pairNumber);
             Debug.Assert(testPair1.majorColor == Color.White);
             Debug.Assert(testPair1.minorColor == Color.Brown);
 
             pairNumber = 5;
-            testPair1 = map.GetColorFromPairNumber(pairNumber);
-            DisplayResultOfGetColorFromPairNumber(pairNumber, testPair1);
+            testPair1 = CallAndPrintResultOfGetColorFromPairNumber(pairNumber);
             Debug.Assert(testPair1.majorColor == Color.White);
             Debug.Assert(testPair1.minorColor == Color.SlateGray);
 
             pairNumber = 23;
-            testPair1 = map.GetColorFromPairNumber(pairNumber);
-            DisplayResultOfGetColorFromPairNumber(pairNumber, testPair1);
+            testPair1 = CallAndPrintResultOfGetColorFromPairNumber(pairNumber);
             Debug.Assert(testPair1.majorColor == Color.Violet);
             Debug.Assert(testPair1.minorColor == Color.Green);
 
             ColorPair testPair2 = new ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
-            pairNumber = map.GetPairNumberFromColor(testPair2);
-            DisplayResultOfGetPairNumberFromColor(pairNumber, testPair2);
+            pairNumber = CallAndPrintResultOfGetPairNumberFromColor(testPair2);
             Debug.Assert(pairNumber == 18);
 
             testPair2 = new ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
-            pairNumber = map.GetPairNumberFromColor(testPair2);
-            DisplayResultOfGetPairNumberFromColor(pairNumber, testPair2);
+            pairNumber = CallAndPrintResultOfGetPairNumberFromColor(testPair2);
             Debug.Assert(pairNumber == 6);
 
             ColorPair pairs = new ColorPair();
             Console.WriteLine("TELECOMMUNICATION COLOR CODE MANUAL");
             pairs.PrintColorPairManual();
         }
-        
+
         /// <summary>
-        /// Display the result of GetColorFromPairNumber output
+        /// Call and display the result of GetColorFromPairNumber
         /// </summary>
-        private static void DisplayResultOfGetColorFromPairNumber(int pairNumber, ColorPair testPair)
+        private static ColorPair CallAndPrintResultOfGetColorFromPairNumber(int pairNumber)
         {
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair);
+            ColorPair testPair1 = new ColorMap().GetColorFromPairNumber(pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
+            return testPair1;
         }
-        
+
         /// <summary>
-        /// Display the result of GetPairNumberFromColor output
+        /// Call and display the result of GetPairNumberFromColor
         /// </summary>
-        private static void DisplayResultOfGetPairNumberFromColor(int pairNumber, ColorPair testPair)
+        private static int CallAndPrintResultOfGetPairNumberFromColor(ColorPair testPair)
         {
+            int pairNumber = new ColorMap().GetPairNumberFromColor(testPair);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair, pairNumber);
+            return pairNumber;
         }
     }
 }
